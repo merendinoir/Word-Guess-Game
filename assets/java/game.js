c73b4bb11b1b90688ec776e910004478d1402295
Isabella Merendino <isabellamerendino@Isabellas-MBP.attlocal.net>
2019-04-04 18:02:41 -0400

    Words to guess function working

--- assets/java/game.js
+++ assets/java/game.js
@@ -21,7 +21,28 @@ var guessesLeft = 10;
 
 function startGame () {
     secretWord = leslieFavThings[Math.floor(Math.random() * leslieFavThings.length)];
+    lettersinWord = secretWord.split("");
+    numBlanks = lettersinWord.length;
+
+    //Reset 
+    guessesLeft = 10;
+    wrongGuesses = [];
+    blanksAndSucesses = [];
+
+    //Drop right number of blanks and successes 
+    for (var i = 0; i < numBlanks; i++) {
+        blanksAndSucesses.push("_");
+    }
+
+    //Push data into the HTML
+    document.getElementById("wordsToGuess").innerHTML = blanksAndSucesses.join(" ");
+    
+    
+    //Testing
     console.log(secretWord);
+    console.log(lettersinWord)
+    console.log(numBlanks)
+    console.log(blanksAndSucesses)
 }
 
 // Main Process
